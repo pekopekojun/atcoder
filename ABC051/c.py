0992abaeb0227cd4fe58800055089e223a81be74
peko_jun <jun805@gmail.com>
2021-02-11 00:43:46 +0900

    Added: ABC051 c

---
+++ ABC051/c.py
@@ -0,0 +1,9 @@
+sx, sy, tx, ty = map(int, input().split())
+
+dx = tx-sx
+dy = ty-sy
+
+print("U"*dy + "R"*dx, end="")
+print("D"*dy + "L"*dx, end="")
+print("L" + "U"*(dy+1) + "R"*(dx+1) + "D", end="")
+print("R" + "D"*(dy+1) + "L"*(dx+1) + "U", end="")
